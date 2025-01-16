897b75fbe85c9d11068bcf732e8043494618c46d
Senanur Yavuzarslan <yavuzarslansenanur@gmail.com>
2025-01-16 19:36:57 +0300

    Add project files.

---
+++ Practice_Polymorphism/BaseClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Practice_Polymorphism
+{
+
+    public class BaseClass
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public virtual double CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    public class Square : BaseClass
+    {
+
+    }
+
+    public class Rectangle : BaseClass
+    {
+
+    }
+
+    public class Triangle : BaseClass
+    {
+        public override double CalculateArea()
+        {
+            return Width * Height / 2;
+        }
+    }
+}
---
+++ Practice_Polymorphism/Program.cs
@@ -0,0 +1,24 @@
+﻿using Practice_Polymorphism;
+
+Square square = new Square()
+{
+    Height = 4,
+    Width = 4
+};
+
+Rectangle rectangle = new Rectangle()
+{
+    Height = 8,
+    Width = 4,
+};
+
+Triangle triangle = new Triangle()
+{
+    Height = 6,
+    Width = 4
+};
+
+Console.WriteLine("Square Area: " + square.CalculateArea());
+Console.WriteLine("Rectangle Area: " + rectangle.CalculateArea());
+Console.WriteLine("Triangle Area: " + triangle.CalculateArea());
+
